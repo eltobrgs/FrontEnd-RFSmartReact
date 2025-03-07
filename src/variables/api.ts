2598c01c8d9a04e51e66952a09cbc3c780937653
elto borges <eltobrgs@MacBook-Air-de-elto.local>
2025-03-07 06:24:13 -0300

    commit de teste

--- src/variables/api.ts
+++ src/variables/api.ts
@@ -1,2 +1,5 @@
-// Arquivo de configuração para variáveis globais da API
 export const API_BASE_URL = 'https://backend-rfsmart.onrender.com/api'; 
+
+//para teste
+//export const API_BASE_URL = 'http://localhost:3000/api';
+
