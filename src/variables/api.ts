d246ab96f5be3a2e1ca6b3b9bee1a27bda6c8509
elto borges <eltobrgs@gmail.com>
2025-03-31 13:07:59 -0300

    mudanças no funcionamneto do sistema de progresso e tamanho dos modulos2

--- src/variables/api.ts
+++ src/variables/api.ts
@@ -1,6 +1,6 @@
 //para produçao 
-// export const API_BASE_URL = 'https://backend-rfsmart.onrender.com/api'; 
+export const API_BASE_URL = 'https://backend-rfsmart.onrender.com/api'; 
 
 //para teste
-export const API_BASE_URL = 'http://localhost:3000/api';
+// export const API_BASE_URL = 'http://localhost:3000/api';
 
